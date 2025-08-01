31608e513cd2518a92869b287d4ac9c4c7eb7632
bata515 <longhui515@gmail.com>
2025-08-01 17:02:54 +0900

    STDIO MCPサーバー作成

--- src/main/java/com/example/mcp_server/McpServerApplication.java
+++ src/main/java/com/example/mcp_server/McpServerApplication.java
@@ -1,9 +1,14 @@
 package com.example.mcp_server;
 
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-import main.java.com.example.mcp_server.WeatherService;
+import com.example.mcp_server.WeatherService;
+
+
 
 @SpringBootApplication
 public class McpServerApplication {
--- src/main/java/com/example/mcp_server/WeatherService.java
+++ src/main/java/com/example/mcp_server/WeatherService.java
@@ -1,3 +1,5 @@
+package com.example.mcp_server;
+
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
