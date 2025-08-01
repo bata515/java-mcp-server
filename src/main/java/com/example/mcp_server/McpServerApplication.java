1078aef1818ee81376ae6b65b488f023e572230a
bata515 <longhui515@gmail.com>
2025-08-01 16:20:50 +0900

    サーバーの構築とアプリケーションの作成

--- src/main/java/com/example/mcp_server/McpServerApplication.java
+++ src/main/java/com/example/mcp_server/McpServerApplication.java
@@ -3,11 +3,18 @@ package com.example.mcp_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import main.java.com.example.mcp_server.WeatherService;
+
 @SpringBootApplication
 public class McpServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(McpServerApplication.class, args);
+	}
+
+	@Bean
+	public ToolCallbackProvider weatherTools(WeatherService weatherService) {
+		return MethodToolCallbackProvider.builder().toolObjects(weatherService).build();
 	}
 
 }
